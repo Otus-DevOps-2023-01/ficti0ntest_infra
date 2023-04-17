030d62ab6b08e3acdc7976e0cf63593a84731787
Александр Нечипоренко <anechiporenko@astralinux.ru>
2023-04-17 11:46:55 +0300

    Ansible 2

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -4,3 +4,7 @@ output "external_ip_address_app" {
 output "external_ip_address_db" {
   value = module.db.external_ip_address_db
 }
+
+output "db_internal_ip" {
+  value = module.db.db_internal_ip
+}
